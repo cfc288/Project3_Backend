ce80cd30519e9b60cf843e072ea8cbbcc8ddbacb
adigi-1775 <adigi1775@gmail.com>
2021-10-23 11:09:02 -0400

    appointment schema updated

--- models/Appointment.js
+++ models/Appointment.js
@@ -1,10 +1,13 @@
 const mongoose = require('mongoose')
 const appointmentSchema = new mongoose.Schema({
+ firstName: {type: String},
+ lastName: {type: String},
  date:{
    day:{type: Number},
    month:{type: Number},
    year:{type: Number},
- }
+ },
+ time: {type: Number},
 })
 
 const Appointment = mongoose.model('Appointment', appointmentSchema)
